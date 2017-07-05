e1a622e6042dc46db9d6e3fc37e795c3d7c30e9f
doberman7 <ivanrubioangeles@gmail.com>
2017-07-05 00:26:10 -0500

    test

---
+++ jquery_tomando_control.js
@@ -0,0 +1,18 @@
+// pagina del sat
+<div id="s4-workspace">
+$("#s4-workspace").css("background-color: black");
+
+$("#s4-workspace").attr("background-color", "rgba(250, 250, 25, 230)")
+
+$("#s4-workspace").attr("width","500")
+
+
+<a class="map_link" id="map_link_0" title="Secretaría de Hacienda y Crédito Público" href="http://www.shcp.gob.mx" target="_blank">Secretaría de Hacienda y Crédito Público</a>
+$("#map_link_0").attr("width", "2px")
+
+// pagina locomotion
+<div class="collapse navbar-collapse" id="main-header">
+$("#main-header").css("width")
+$("#main-header").attr("width", "2px")
+$("#main-header").css("background-color");
+$("body > div.container > div:nth-child(3) > div.col-md-7 > h3") .css("color")
